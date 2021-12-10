6c560f9b346e1d9053f24bc8801dff1ab2ce83e9
root <root@DESKTOP-H2FTGHA.localdomain>
2021-12-10 22:48:14 +0800

    First Commit

---
+++ index.php
@@ -0,0 +1,3 @@
+<?PHP
+  	echo "PHP InstallationIn installing the PHP, I use the Ubuntu Terminal, which is the Linux distribution I choosed. Php 7.0 cant be installed so I searched it online, and I have installed the Php7.4 instead. Apache2 was installed already along with the installation of the Linux distro of my choice.";
+?>
